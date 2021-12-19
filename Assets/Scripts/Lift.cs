30109e7e38582cf25f3cc5258d4d3006e643943f
dinikai <dinikaigd@gmail.com>
2021-12-19 16:24:26 +0700

    fcking commit

---
+++ Assets/Scripts/Lift.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lift : MonoBehaviour
+{
+    [SerializeField] private Button useButton;
+    public Vector2 vector;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            useButton.interactable = true;
+            ItemUse.SelectedItem = "lift";
+            ItemUse.liftVector = vector;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            useButton.interactable = false;
+        }
+    }
+}
